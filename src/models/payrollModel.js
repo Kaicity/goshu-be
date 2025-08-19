9f23941fe0c66a16c85fc77fb1fd4cd779f60eb8
Nguyễn Minh Thông <nguyenminhthongITMIX@gmail.com>
2025-08-19 23:22:50 +0700

    create module api

--- src/models/payrollModel.js
+++ src/models/payrollModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { getCurrentTime } = require('../utils/timeZone');
+const PayrollStatus = require('../enums/payrollStatus');
 
 const PayrollSchema = new mongoose.Schema({
   employeeId: { type: mongoose.Schema.Types.ObjectId, ref: 'employees', required: true },
@@ -10,7 +11,13 @@ const PayrollSchema = new mongoose.Schema({
   overtime: { type: Number, default: 0 },
   deductions: { type: Number, default: 0 },
   netSalary: { type: Number },
+  status: {
+    type: String,
+    enum: [PayrollStatus.OPEN, PayrollStatus.CALCULATED, PayrollStatus.CLOSED],
+    default: PayrollStatus.OPEN,
+  },
   createdAt: { type: Date, default: getCurrentTime() },
+  updatedAt: { type: Date, default: getCurrentTime() },
 });
 
 const PayrollModel = mongoose.model('payrolls', PayrollSchema);
