3635885c1991f0a2754384ba58309501e773feef
Nguyễn Minh Thông <nguyenminhthongITMIX@gmail.com>
2025-08-22 15:21:01 +0700

    refactor: simplify getAllAttendance route by removing authorization middleware

--- src/routers/attendance.router.js
+++ src/routers/attendance.router.js
@@ -1,12 +1,10 @@
 const Router = require('express');
-const { checkIn, checkOut, getAllAttendance, getAllAttendanceByEmployee } = require('../controllers/attendance.controller');
-const authorizeRole = require('../middlewares/authorizeRole');
-const UserRoles = require('../enums/userRoles');
+const { checkIn, checkOut, getAllAttendance } = require('../controllers/attendance.controller');
 
 const attendanceRouter = new Router();
 
 attendanceRouter.post('/checkin', checkIn);
 attendanceRouter.post('/checkout', checkOut);
-attendanceRouter.get('/getAll', authorizeRole(UserRoles.ADMIN, UserRoles.HR), getAllAttendance);
+attendanceRouter.get('/getAll', getAllAttendance);
 
 module.exports = attendanceRouter;
