e54a89ed2a3f5bc00e982d4468930d0465942240
Nguyễn Minh Thông <nguyenminhthongITMIX@gmail.com>
2025-09-02 17:22:03 +0700

    feat: enhance attendance management with monthly generation and update functionality

--- src/routers/attendance.router.js
+++ src/routers/attendance.router.js
@@ -3,8 +3,9 @@ const {
   checkIn,
   checkOut,
   getAllAttendance,
-  generateAttendanceManual,
   deleteAttendanceInMonth,
+  updateAttendance,
+  generateAttendanceManualForMonth,
 } = require('../controllers/attendance.controller');
 const authorizeRole = require('../middlewares/authorizeRole');
 const UserRoles = require('../enums/userRoles');
@@ -14,7 +15,8 @@ const attendanceRouter = express.Router();
 attendanceRouter.post('/checkin', authorizeRole(UserRoles.EMPLOYEE, UserRoles.HR), checkIn);
 attendanceRouter.post('/checkout', authorizeRole(UserRoles.EMPLOYEE, UserRoles.HR), checkOut);
 attendanceRouter.get('/getAll', authorizeRole(UserRoles.EMPLOYEE, UserRoles.HR), getAllAttendance);
-attendanceRouter.post('/generateAttendances', authorizeRole(UserRoles.HR), generateAttendanceManual);
+attendanceRouter.post('/generateAttendances', authorizeRole(UserRoles.HR), generateAttendanceManualForMonth);
 attendanceRouter.delete('/deleteAttendances/:year/:month', authorizeRole(UserRoles.HR), deleteAttendanceInMonth);
+attendanceRouter.post('/updateAttendance', authorizeRole(UserRoles.HR), updateAttendance);
 
 module.exports = attendanceRouter;
